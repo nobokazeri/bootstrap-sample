5bf710eb2ddca57ea5447175467bb719fe69d0ab
nobokazeri <nobokazeri@gmail.com>
2019-02-22 04:39:37 +0900

    develop

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+const gulp = require('gulp')
+const ejs = require("gulp-ejs")
+const sass = require('gulp-sass')
+const postcss = require('gulp-postcss')
+const autoprefixer = require('autoprefixer')
+const flexBugsFixes = require('postcss-flexbugs-fixes')
+
+const autoprefixerOption = {
+  grid: true
+}
+
+const postcssOption = [
+  flexBugsFixes,
+  autoprefixer(autoprefixerOption)
+]
+
+const ejsSettingOption = {
+  ext: '.html'
+}
+
+gulp.task('sass', () => {
+  return gulp.src('./src/scss/common.scss')
+    .pipe(sass())
+    .pipe(postcss(postcssOption))
+    .pipe(gulp.dest('./public'))
+})
+
+gulp.task('ejs', () => {
+  return gulp
+    .src('./src/html/*.ejs')
+    .pipe(ejs({}, {}, ejsSettingOption))
+    .pipe(gulp.dest('./public'))
+})
+
+gulp.task('watch', () => {
+   gulp.watch('./src/sass/**/*.scss', gulp.series('sass'))
+   gulp.watch('./src/html/atoms/**/*.ejs', gulp.series('ejs'))
+   gulp.watch('./src/html/molecules/**/*.ejs', gulp.series('ejs'))
+   gulp.watch('./src/html/organisms/**/*.ejs', gulp.series('ejs'))
+   gulp.watch('./src/html/*.ejs', gulp.series('ejs'))
+})
